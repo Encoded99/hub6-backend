aa1174a4f3c1e3619badc099f10046d80c60b824
Ismail Umar <119731277+Encoded99@users.noreply.github.com>
2024-03-23 18:38:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cors());
 
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://hub6.vercel.app', credentials: true }));
 
 
 
